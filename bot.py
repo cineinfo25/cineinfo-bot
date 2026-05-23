09f54d798624d2b38806549e88f3ec85274f87ab
cineinfo25 <gtejas358@gmail.com>
2026-05-24 00:15:27 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import os
 from telegram import Update
 from telegram.ext import Application, MessageHandler, CommandHandler, ContextTypes, filters
 
-TELEGRAM_TOKEN = os.getenv("8851255730:AAG3Tfk_lZHiAb5oKPkUiTwHN-DVFHsHg6w")
+TELEGRAM_TOKEN = os.getenv("8851255730:AAGAdcNqzuIgTFJa96RuBRyYNvbXi5BmwNs")
 GEMINI_API_KEY = os.getenv("AIzaSyD531RXj134QhsO8ceTiZh6YWrl74NQkIk")
 
 logging.basicConfig(level=logging.INFO)
